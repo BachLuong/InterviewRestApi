58eb81a7edaeee3f3a5b619d84ea7cf8e15ed406
BachLuong <luongbv1989@gmail.com>
2020-02-21 22:15:00 +0700

    add tomcat war support

--- src/main/java/com/vn/canalcircle/InterviewRestApi/InterviewRestApiApplication.java
+++ src/main/java/com/vn/canalcircle/InterviewRestApi/InterviewRestApiApplication.java
@@ -2,9 +2,10 @@ package com.vn.canalcircle.InterviewRestApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InterviewRestApiApplication {
+public class InterviewRestApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterviewRestApiApplication.class, args);
