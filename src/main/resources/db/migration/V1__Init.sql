f8c633d89112f034f7bde2ac39c24527f583636a
BachLuong <luongbv1989@gmail.com>
2020-02-21 22:03:11 +0700

    edit mysql init data

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -4,7 +4,7 @@ CREATE TABLE `product`  (
   `product_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `product_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`product_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 INSERT INTO `product` (product_name,product_description) VALUES ('iPad', 'Apple product');
 INSERT INTO `product` (product_name,product_description) VALUES ('Note 10', 'Samsung product');
